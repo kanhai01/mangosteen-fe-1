d8b4bf438aac08bd30e5896c5354039c6aaf6178
1572946137@qq.com <1572946137@qq.com>
2022-11-24 21:08:02 +0800

    使用 TSX 代替Template

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueJsx({
+      transformOn: true,
+      mergeProps: true,
+    }),
+  ],
+  // plugins:[
+  //   vueJsx({
+  //     transformOn = true,
+  //     mergeProps = true
+  //   })
+  // ]
 });
