d354fdd36900bc97ca44542467e7fa0f2f5e08a3
1572946137@qq.com <1572946137@qq.com>
2022-11-28 22:38:49 +0800

    更新默认路由

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -2,17 +2,18 @@ import { first } from "../components/welcome/First";
 import { forth } from "../components/welcome/Forth";
 import { second } from "../components/welcome/Second";
 import { third } from "../components/welcome/Third";
-import { Bar } from "../views/Bar";
-import { Foo } from "../views/Foo";
 import { Welcome } from "../views/Welcome";
 
 export const routes = [
-  { path: "/Foo", component: Foo },
-  { path: "/Bar", component: Bar },
+  { path: "/", redirect: "/welcome" },
   {
     path: "/welcome",
     component: Welcome,
     children: [
+      {
+        path:'',
+        redirect:'/welcome/1'
+      },
       {
         path: "1",
         component: first,
