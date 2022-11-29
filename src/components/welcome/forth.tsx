8fcd3ebd57913372b26ab216adbbb80a96f6cc8a
1572946137@qq.com <1572946137@qq.com>
2022-11-29 22:55:46 +0800

    优化重构

--- src/components/welcome/forth.tsx
+++ src/components/welcome/forth.tsx
@@ -1,38 +1,35 @@
-import { defineComponent } from "vue";
 import s from "./WelcomeLayout.module.scss";
 import welcome4 from "../../assets/icons/welcome4.svg";
 import { RouterLink } from "vue-router";
 import { WelcomeLayout } from "./WelcomeLayout";
 
-export const forth = defineComponent({
-  setup: (props, context) => {
-    return () => (
-      <WelcomeLayout>
-        {{
-          icon: () => <img src={welcome4} />,
-          title: () => (
-            <h2>
-              云备份
-              <br />
-              再也不怕数据丢失
-            </h2>
-          ),
-          buttons: () => (
-            <>
-              {" "}
-              <RouterLink class={s.fake} to="/start">
-                跳过
-              </RouterLink>
-              <RouterLink class={s.next} to="/start">
-                完成
-              </RouterLink>
-              <RouterLink class={s.fake} to="/start">
-                跳过
-              </RouterLink>
-            </>
-          ),
-        }}
-      </WelcomeLayout>
-    );
-  },
-});
+export const forth = {
+  render: () => (
+    <WelcomeLayout>
+      {{
+        icon: () => <img src={welcome4} />,
+        title: () => (
+          <h2>
+            云备份
+            <br />
+            再也不怕数据丢失
+          </h2>
+        ),
+        buttons: () => (
+          <>
+            {" "}
+            <RouterLink class={s.fake} to="/start">
+              跳过
+            </RouterLink>
+            <RouterLink class={s.next} to="/start">
+              完成
+            </RouterLink>
+            <RouterLink class={s.fake} to="/start">
+              跳过
+            </RouterLink>
+          </>
+        ),
+      }}
+    </WelcomeLayout>
+  ),
+};
