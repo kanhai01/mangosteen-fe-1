ee4b53b753b26db8d43d09ebbe1f26d68bb6ea93
1572946137@qq.com <1572946137@qq.com>
2022-12-04 14:30:22 +0800

    添加StartPage页面和Button组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,10 +1,16 @@
 import { defineComponent } from "vue";
+import s from "./StartPage.module.scss";
 import { Button } from "../shared/Button";
 export const StartPage = defineComponent({
   setup: (props, context) => {
+    const onClick = () => {
+      console.log("onClick");
+    };
     return () => (
-      <div>
-        <Button>测试11111111</Button>
+      <div class={s.button_wrapper}>
+        <Button class={s.button} onClick={onClick}>
+          开始记账
+        </Button>
       </div>
     );
   },
